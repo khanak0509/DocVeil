742fc5d5a583122ff154e937ae7a1445bcf5df44
khanak0509 <kkhanak512@gmail.com>
2025-12-17 17:20:54 +0530

    schema for image

--- backend/healper_function.py
+++ backend/healper_function.py
@@ -2,12 +2,19 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_ollama import ChatOllama
 from langchain_core.prompts import PromptTemplate
 import asyncio
+from pydantic import BaseModel
+from typing import List, Dict
 
 llm = ChatOllama(
     model="llama3.1:8b",
     temperature=0.3
 )
 
+
+class Image(BaseModel):
+    image_name : List[str] = []
+
+llm_stuc = llm.with_structured_output(Image)
 
 async def summery_asycn(page_contnet:str):
     prompt = PromptTemplate(
@@ -16,8 +23,32 @@ async def summery_asycn(page_contnet:str):
 you are a sumuarizer ow genegrate a detiald summeruot of {page_contnet}
 """
     )
-    chain = prompt | llm 
+    chain = prompt | llm
     result = await chain.ainvoke({'page_contnet' : page_contnet})
     return result.content
+
+
+async def image(summary_text:str) -> Image:
+    propmt_for_image = PromptTemplate(
+        input_variables=["summary_text"],
+        template="""
+You are an expert visual content designer. Read the following summary and suggest only 1 highly relevant, creative, and descriptive image names that would best illustrate or visually represent the key ideas, concepts, or themes in the summary. 
+
+Guidelines:
+- Use concise, vivid, and specific phrases (not generic words)
+- Focus on the main topics, actions, or entities described
+- Avoid repeating the summary text verbatim
+- Do not include file extensions or numbers (e.g., no .jpg, no 'Image 1')
+- Only output a Python list of image names (e.g., ['Data Flow Diagram', 'Legal Contract Review'])
+you can say none also 
+
+Summary:
+{summary_text}
+"""
+    )
+    chain = propmt_for_image | llm_stuc
+    return await chain.ainvoke({"summary_text": summary_text})
+
+    
 
 
