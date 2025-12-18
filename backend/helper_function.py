aec42cb8485e0432c1c7de7fd21f60648eb9b7fb
khanak0509 <kkhanak512@gmail.com>
2025-12-18 20:57:52 +0530

    remove image function

--- backend/helper_function.py
+++ backend/helper_function.py
@@ -64,26 +64,26 @@ IMPORTANT:
     return result.content
 
 
-async def get_image_metadata(summary_text:str) -> Image:
-    prompt_for_image = PromptTemplate(
-        input_variables=["summary_text"],
-        template="""
-You are an expert visual content designer. Read the following summary and suggest only 1 highly relevant, creative, and descriptive image names that would best illustrate or visually represent the key ideas, concepts, or themes in the summary. 
+# async def get_image_metadata(summary_text:str) -> Image:
+#     prompt_for_image = PromptTemplate(
+#         input_variables=["summary_text"],
+#         template="""
+# You are an expert visual content designer. Read the following summary and suggest only 1 highly relevant, creative, and descriptive image names that would best illustrate or visually represent the key ideas, concepts, or themes in the summary. 
 
-Guidelines:
-- Use concise, vivid, and specific phrases (not generic words)
-- Focus on the main topics, actions, or entities described
-- Avoid repeating the summary text verbatim
-- Do not include file extensions or numbers (e.g., no .jpg, no 'Image 1')
-- Only output a Python list of image names (e.g., ['Data Flow Diagram', 'Legal Contract Review'])
-You can say none also 
+# Guidelines:
+# - Use concise, vivid, and specific phrases (not generic words)
+# - Focus on the main topics, actions, or entities described
+# - Avoid repeating the summary text verbatim
+# - Do not include file extensions or numbers (e.g., no .jpg, no 'Image 1')
+# - Only output a Python list of image names (e.g., ['Data Flow Diagram', 'Legal Contract Review'])
+# You can say none also 
 
-Summary:
-{summary_text}
-"""
-    )
-    chain = prompt_for_image | llm_stuc
-    return await chain.ainvoke({"summary_text": summary_text})
+# Summary:
+# {summary_text}
+# """
+#     )
+#     chain = prompt_for_image | llm_stuc
+#     return await chain.ainvoke({"summary_text": summary_text})
 
     
 
